c97b1caf64ab2426b511c3ab1233b47410d5a55c
Stephen Harb <stephen.harb@ironpixietechnolgoies.com>
2019-07-15 19:01:07 -0500

    sss

---
+++ backend/src/main/java/controller/NotComplexController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class NotComplexController{
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test()
+    {
+        return "TEST PASSED";
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String Status()
+    {
+        return "STATUS!";
+    }
+}
